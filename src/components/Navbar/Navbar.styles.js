cd04ff7840b3460c8845a238d5600e25dc8ff1cb
lmjcbs <hello@liamjacobs.dev>
2020-03-01 16:38:41 +0000

    added navbar with base nav links

---
+++ src/components/Navbar/Navbar.styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  textDecoration: 'none',
+  color: 'white'
+};
+
+export const NavbarWrapper = styled.div`
+  height: 20px;
+  margin: 0px;
+  padding: 20px;
+  width: 100%;
+  background-color: lightslategrey;
+`;
