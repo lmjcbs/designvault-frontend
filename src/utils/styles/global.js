1c5198694390fd075d9d240bcce81e234aa5be73
lmjcbs <hello@liamjacobs.dev>
2020-03-01 18:17:47 +0000

    add global from components

---
+++ src/utils/styles/global.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Form = styled.form``;
+
+export const Input = styled.input`
+  font-size: 18px;
+  color: blue;
+`;
+
+export const Label = styled.label`
+  font-size: 18px;
+  color: black;
+`;
+
+export const MainHeader = styled.h1`
+  font-size: 24px;
+  color: red;
+`;
