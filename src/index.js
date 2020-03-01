ce8ac3e3ab9f9a4d206628d5dfb0848f8838a24a
lmjcbs <hello@liamjacobs.dev>
2020-03-01 23:15:27 +0000

    wrap app in provider passing store info

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
 import { Navbar, Login, Register } from './components/index';
 import './index.css';
 
 ReactDOM.render(
-  <Router>
-    <Navbar />
-    <Route path="/login" component={Login} />
-    <Route path="/register" component={Register} />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <Navbar />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
