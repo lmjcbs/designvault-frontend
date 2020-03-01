59f25e57b2fbe1bf52b6d5849aa5ea19595ca4c7
lmjcbs <hello@liamjacobs.dev>
2020-03-01 18:17:29 +0000

    add custom hook for form data input

---
+++ src/utils/hooks/useFormInput.hook.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+export const useFormInput = initialValue => {
+  const [value, setValue] = useState(initialValue);
+
+  const handleChange = e => {
+    setValue(e.target.value);
+  };
+
+  return {
+    value,
+    onChange: handleChange
+  };
+};
