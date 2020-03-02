515ee7e3e01187e01ded8c91af116e8062eb8a30
lmjcbs <hello@liamjacobs.dev>
2020-03-02 12:39:53 +0000

    add redirect to previous location after login

--- src/components/Login/Login.container.jsx
+++ src/components/Login/Login.container.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import LoginForm from './LoginForm.component';
 import { MainHeader } from '../../utils/styles/global';
+import { Redirect, useLocation } from 'react-router';
+import { connect } from 'react-redux';
 
-const Login = () => {
-  return (
+const Login = ({ isAuthenticated }) => {
+  const location = useLocation();
+
+  return isAuthenticated ? (
+    <Redirect to={{ pathname: location.state.from.pathname }} />
+  ) : (
     <>
       <MainHeader>Login</MainHeader>
       <LoginForm></LoginForm>
@@ -11,4 +17,8 @@ const Login = () => {
   );
 };
 
-export default Login;
+const mapStateToProps = (state) => {
+  return { isAuthenticated: state.authReducer.isAuthenticated };
+};
+
+export default connect(mapStateToProps)(Login);
