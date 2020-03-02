982e33b1485f525f4b5b1bcbda15bc7dcb6c3d46
lmjcbs <hello@liamjacobs.dev>
2020-03-02 12:40:32 +0000

    add base home component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Navbar from './Navbar/Navbar.container';
 import Login from './Login/Login.container';
 import Register from './Register/Register.container';
+import Home from './Home/Home.container';
 
-export { Navbar, Login, Register };
+export { Navbar, Login, Register, Home };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import PrivateRoute from './utils/PrivateRoute.component';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import { Navbar, Login, Register } from './components/index';
+import { Navbar, Login, Register, Home } from './components/index';
 import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
       <Navbar />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+      <PrivateRoute exact path="/" component={Home} />
     </Router>
   </Provider>,
   document.getElementById('root')
