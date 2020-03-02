73d9858a069d29355149aa605decff6de2e26716
lmjcbs <hello@liamjacobs.dev>
2020-03-02 15:16:19 +0000

    handle registering new user

--- src/actions/Actions.js
+++ src/actions/Actions.js
@@ -1,4 +1,9 @@
-import { LOADING_USER, LOGIN_USER } from './ActionTypes';
+import {
+  LOADING_USER,
+  LOGIN_USER,
+  REGISTERING_USER,
+  REGISTER_USER
+} from './ActionTypes';
 
 export const userLoginAction = (userObj) => ({
   type: LOGIN_USER,
@@ -24,6 +29,37 @@ export const userLoginFetchAction = (userObj) => async (dispatch) => {
       }
     );
     const user = await response.json();
+    dispatch(userLoginAction(user));
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const userRegisterAction = () => ({
+  type: REGISTER_USER
+});
+
+export const userRegisteringAction = () => ({
+  type: REGISTERING_USER
+});
+
+export const userRegisterFetchAction = (userObj) => async (dispatch) => {
+  dispatch(userRegisterAction());
+  try {
+    const response = await fetch(
+      'http://localhost:3000/api/v1/users/register',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json'
+        },
+        body: JSON.stringify(userObj)
+      }
+    );
+    const user = await response.json();
+    console.log(user);
+    dispatch(userRegisterAction());
     dispatch(userLoginAction(user));
   } catch (error) {
     throw error;
--- src/components/Register/RegisterForm.component.jsx
+++ src/components/Register/RegisterForm.component.jsx
@@ -1,41 +1,57 @@
 import React from 'react';
-import { Form, Input, Label } from '../../utils/styles/global';
+import { connect } from 'react-redux';
+import { Form, Input, Label, MainHeader } from '../../utils/styles/global';
 import { useFormInput } from '../../utils/hooks/useFormInput.hook';
+import { userRegisterFetchAction } from '../../actions/Actions';
 
-const RegisterForm = () => {
+const RegisterForm = ({ userRegisterFetchAction }) => {
   const email = useFormInput('');
   const password = useFormInput('');
   const passwordConfirmation = useFormInput('');
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    userRegisterFetchAction({
+      user: {
+        email: email.value,
+        password: password.value,
+        password_confirmation: passwordConfirmation.value
+      }
+    });
+  };
+
   return (
-    <Form>
-      <Label htmlFor="email">Email</Label>
-      <Input
-        type="text"
-        name="email"
-        placeholder="Email"
-        {...email}
-      ></Input>
-      <br />
-      <Label htmlFor="password">Password</Label>
-      <Input
-        type="password"
-        name="password"
-        placeholder="Password"
-        {...password}
-      ></Input>
-      <br />
-      <Label htmlFor="passwordConfirmation">Confirm Password</Label>
-      <Input
-        type="password"
-        name="passwordConfirmation"
-        placeholder="Enter Password Again"
-        {...passwordConfirmation}
-      ></Input>
-      <br />
-      <Input type="submit" value="Register"></Input>
-    </Form>
+    <>
+      <MainHeader>Register</MainHeader>
+      <Form onSubmit={handleSubmit}>
+        <Label htmlFor="email">Email</Label>
+        <Input
+          type="text"
+          name="email"
+          placeholder="Email"
+          {...email}
+        ></Input>
+        <br />
+        <Label htmlFor="password">Password</Label>
+        <Input
+          type="password"
+          name="password"
+          placeholder="Password"
+          {...password}
+        ></Input>
+        <br />
+        <Label htmlFor="passwordConfirmation">Confirm Password</Label>
+        <Input
+          type="password"
+          name="passwordConfirmation"
+          placeholder="Enter Password Again"
+          {...passwordConfirmation}
+        ></Input>
+        <br />
+        <Input type="submit" value="Register"></Input>
+      </Form>
+    </>
   );
 };
 
-export default RegisterForm;
+export default connect(null, { userRegisterFetchAction })(RegisterForm);
