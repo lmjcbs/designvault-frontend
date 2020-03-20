43c7739e7ebef7993d08855fdf540a7b02f6ea70
lmjcbs <hello@liamjacobs.dev>
2020-03-20 11:33:11 +0000

    add activity bar container

---
+++ src/components/Home/ActivityBar.container.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ActivityWrapper } from './Home.styles';
+
+const ActivityBar = () => {
+  return (
+    <ActivityWrapper>
+      <h2>Discover</h2>
+    </ActivityWrapper>
+  );
+};
+
+export default ActivityBar;
