8db1d9176e999cb0d4e7c179a56552303320b1ad
lmjcbs <hello@liamjacobs.dev>
2020-03-20 11:34:13 +0000

    divide home page into grid columns

--- src/components/Home/Home.container.jsx
+++ src/components/Home/Home.container.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import PostForm from './PostForm.component';
 import Feed from './Feed.container';
+import { Row, Col } from '../../utils/styles/global';
+import ProfileBar from './ProfileBar.container';
+import ActivityBar from './ActivityBar.container';
 
 const Home = () => {
   return (
     <>
-      <h1>Protected Page</h1>
-      <PostForm></PostForm>
-      <Feed></Feed>
+      <Row>
+        <Col size={1}>
+          <ProfileBar></ProfileBar>
+        </Col>
+        <Col size={2}>
+          <h1>Protected Page</h1>
+          <PostForm></PostForm>
+          <Feed></Feed>
+        </Col>
+        <Col size={1}>
+          <ActivityBar></ActivityBar>
+        </Col>
+      </Row>
     </>
   );
 };
